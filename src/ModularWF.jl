186b074e70ac613034d5a938c2016322a0b1cd1f
Ben Elkin <8xe8n-cm@online.de>
2023-10-03 21:58:01 +0200

    getnames return (;modules, packages, constants...)

--- src/ModularWF.jl
+++ src/ModularWF.jl
@@ -1,5 +1,9 @@
 module ModularWF
 
+const kwargs0 = (; functions=true, types=true, constants=true, vars=false, modules=false, packages=true, all=false, none=false)
+const dontimport = ( :Base, :Core, :InteractiveUtils, :Main, :Revise, :VSCodeServer)
+const notavar = (:ans, :eval, :include)
+
 function makeexpr_allnames(modname)
     pckname = @__MODULE__
     s = 
@@ -61,9 +65,31 @@ function istypedglobal(m, v)
     return (; isvar = true, btype, t_glob)
 end
 
-
-istypedvar(m, v) = isdefined(m, v) && 
+"""
+    istypedvar(m, v)
+returns `true` if v is either `const` or typed global in the module `v`  
+"""
+istypedvar(m, v) = isdefined(m, v) &&  
     ((isconst(m, v) && !(getproperty(m, v) isa Function)) ||
     Core.get_binding_type(m, v) != Any)
+
+
+function filter_names(n)
+    n = [nn for nn in n if !startswith(string(nn), "#")]
+    return setdiff(n, dontimport, notavar)
+end
+
+function getnames(m=Main)
+    n0 = filter_names(names(m; all=true, imported=false))
+    n1 = filter_names(names(m; all=true, imported=true))
+    imported = setdiff(n1, n0)
+    modules = [n for n in n0 if getproperty(m, n) isa Module]
+    packages = [n for n in imported if getproperty(m, n) isa Module]
+    functions = [n for n in n1 if getproperty(m, n) isa Function]
+    constants = setdiff([n for n in n1 if isconst(m, n)], functions, packages, modules)
+    typedglobals = [n for n in n1 if istypedglobal(m, n).t_glob]
+    vars = setdiff(n1, modules, packages, constants, typedglobals, functions)
+    return (;modules, packages, constants, typedglobals, functions, vars)
+end
 
 end
